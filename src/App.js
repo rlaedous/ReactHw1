8125f1af83939f6a6cfa873942674d151d4c2489
rlaeodus <gwg03185@gmail.com>
2024-01-18 00:47:49 +0900

    뼈대 대충 작성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // 카드 추가 버튼
+  const addCardButton = () =>{
+    alert("추가")
+  }
+  // 카드 삭제 버튼
+  const deleteButtonHandler = () =>{
+    alert("삭제")
+  } 
+  // 카드 완료 버튼 
+  const completeButton = () =>{
+    alert("완료")
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='page-style'>
+      <span>My Todo List</span> 
+      <span>React</span>
+      <div>제목<input/>내용<input/>
+      <button onClick={addCardButton}>추가하기</button></div>
+        
+      <div>Working</div>
+      <div className='card-style'>
+        <div style={{fontSize: 20}}>리액트 공부하기</div>
+        <div style={{fontSize: 15}}>리액트 기초를 공부합니다.</div>
+        <div> 
+        <button className='deleteButton' onClick={deleteButtonHandler}>삭제하기</button>
+        <button className='completeButton' onClick={completeButton}>완료</button>
+        </div>
+      </div>
+      
+      <div>Done</div>
+      </div>
   );
 }
 
